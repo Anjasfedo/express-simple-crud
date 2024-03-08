b4b66c13c09e128d73eae00e90eb94b709b656cb
AnjasFedo <fedoaf98@gmail.com>
2024-03-08 20:33:10 +0700

    Install Express.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+    console.log("Run on port 3000")
+});
