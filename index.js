83da8f6e1489018baa5a58479ec4cc319401a3f0
AnjasFedo <fedoaf98@gmail.com>
2024-03-08 20:58:32 +0700

    Install Mongoose & Init MongoDB Atlas

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Connected!"))
+  .catch(() => console.log("Not Connected"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.listen(3000, () => {
-    console.log("Run on port 3000")
+  console.log("Run on port 3000");
 });
