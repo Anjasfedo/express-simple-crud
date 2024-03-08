895bd6e21cb61d0357e92cd5571934c332716383
AnjasFedo <fedoaf98@gmail.com>
2024-03-08 21:14:35 +0700

    The POST Method

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/product.model");
 require("dotenv").config();
+
+app.use(express.json());
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
@@ -12,6 +15,16 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(3000, () => {
