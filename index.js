6b1975e3ce9a0d304d632dbacfdb8ed58a9d2c40
AnjasFedo <fedoaf98@gmail.com>
2024-03-09 22:43:43 +0700

    GET All Products Method

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/products", async (req, res) => {
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
 
