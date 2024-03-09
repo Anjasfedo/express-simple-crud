47339caa08773f1f12069a3458b8c98683c6391b
AnjasFedo <fedoaf98@gmail.com>
2024-03-09 22:48:49 +0700

    GET Product By Id Method

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
